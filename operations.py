62c4e1c2725ef2f6d3230e4024c28ac8945d6789
b-smith94 <byrondsmith9@gmail.com>
2024-09-05 14:43:45 -0400

    added methods for each menu item within the appropriate class. Did not fill out code for each method yet, may also change functions and locations later.

--- operations.py
+++ operations.py
@@ -1,8 +1,45 @@
 class BookOperations:
-    pass
+    def __init__(self, book, author):    
+        self.book = book
+        self.author = author
+    
+    def add_new_book(self, book, author):
+        pass
 
-class UserOperations:
-    pass
+    def borrow_book(self, book, author):
+        pass
 
-class AuthorOperations:
-    pass
+    def return_book(self, book, author):
+        pass
+
+    def search_books(self, book, author):
+        pass
+
+    def display_books(self):
+        pass
+
+class UserOperations(BookOperations):
+    def __init__(self, username, library_id, book):
+        super().__init__(book)
+        self.__username = username
+        self.__library_id = library_id
+
+    def add_new_user(self, username, library_id):
+        print("New user added.")
+        pass
+
+    def view_user_details(self, username):
+        pass
+
+    def display_users(self):
+        pass
+
+class AuthorOperations(BookOperations):
+    def __init__(self, author):
+        super().__init__(author)
+
+    def view_author_details(self, author):
+        pass
+
+    def display_all_authors(self):
+        pass
