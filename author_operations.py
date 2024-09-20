33fcb040e0fb4dc854d04f462baf5808527b9c65
b-smith94 <byrondsmith9@gmail.com>
2024-09-20 10:11:35 -0400

    Finished work updating code to work with a database instead of a dictionary and text file

--- author_operations.py
+++ author_operations.py
@@ -1,26 +1,88 @@
+import connect_mysql
+from connect_mysql import connect_database
+
 class AuthorOperations:
     def __init__(self, author):
         self.author = author
 
     def add_new_author(self, author, birthday, date_of_death):
-        if author not in self:
+        conn = connect_database()
+        if conn is not None:
             try:
-                self[author] = {"Date of Birth": birthday, "Date of Death": date_of_death}
+                cursor = conn.cursor()
+                new_author = author, f"Birthday: {birthday}; Date of Death: {date_of_death}"
+                query = "INSERT INTO authors (name, biography) VALUES (%s, %s)"
+
+                cursor.execute(query, new_author)
+                conn.commit()
+
+                print(f"{author} successfully added to the database!")
+            except connect_mysql.Error as db_err:
+                print(f"A Database Error has occurred: {db_err}")
             except Exception as e:
                 print(f"An error has occurred: {e}")
+            finally:
+                cursor.close()
+                conn.close()
         
         else:
             print(f"{author} already exists in the database.")
     def view_author_details(self, author):
-        try:
-            print(f"Author: {author}")
-            for detail, info in self[author].items():
-                print(f"   {detail}: {info}")
-        except KeyError:
-            print(f"{author} is not currently in our database.")
+        conn = connect_database()
+        if conn is not None:
+            try:
+                cursor = conn.cursor()
+                
+                query = "SELECT * FROM authors WHERE name = %s"
+
+                cursor.execute(query, author)
+
+                for row in cursor.fetchall():
+                    print(row)
+            except connect_mysql.Error as db_err:
+                print(f"A Database Error has occurred: {db_err}")
+            except Exception as e:
+                print(f"An error has occurred: {e}")
+            finally:
+                cursor.close()
+                conn.close()
+
+    def return_author_id(self, author):
+        conn = connect_database()
+        if conn is not None:
+            try:
+                cursor = conn.cursor()
+                
+                query = "SELECT id FROM authors WHERE name = %s"
+
+                cursor.execute(query, author)
+
+                for row in cursor.fetchall():
+                    return row
+            except connect_mysql.Error as db_err:
+                print(f"A Database Error has occurred: {db_err}")
+            except Exception as e:
+                print(f"An error has occurred: {e}")
+            finally:
+                cursor.close()
+                conn.close()
 
     def display_all_authors(self):
-        for author, category in self.items():
-            print(f"Author: {author}")
-            for detail, info in category.items():
-                print(f"   {detail}: {info}")
+        conn = connect_database()
+        if conn is not None:
+            try:
+                cursor = conn.cursor()
+                
+                query = "SELECT * FROM authors"
+
+                cursor.execute(query)
+
+                for row in cursor.fetchall():
+                    print(row)
+            except connect_mysql.Error as db_err:
+                print(f"A Database Error has occurred: {db_err}")
+            except Exception as e:
+                print(f"An error has occurred: {e}")
+            finally:
+                cursor.close()
+                conn.close()
